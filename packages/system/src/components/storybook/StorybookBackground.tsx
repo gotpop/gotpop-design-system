6e3d642540948c3430fe18f795d030b7048884db
GotPop <gotpop@users.noreply.github.com>
2025-11-20 11:44:40 +0000

    Link list stories

--- packages/system/src/components/storybook/StorybookBackground.tsx
+++ packages/system/src/components/storybook/StorybookBackground.tsx
@@ -7,18 +7,16 @@ interface StorybookBackgroundProps {
   className?: string
 }
 
-/**
- * Reusable background component for Storybook tests
- * Provides consistent background styling for component testing
- */
 export function StorybookBackground({
   children,
   variant = "secondary-700",
   className = "",
-}: StorybookBackgroundProps) {
+  style,
+}: StorybookBackgroundProps & { style?: React.CSSProperties }) {
   return (
     <div
       className={`storybook-background storybook-background--${variant} ${className}`}
+      style={style}
     >
       {children}
     </div>

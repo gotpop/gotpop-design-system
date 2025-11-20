13bddec00b458c0722118dd332e266cab4070bbe
GotPop <gotpop@users.noreply.github.com>
2025-11-20 13:49:10 +0000

    Rich text stories

--- packages/system/src/components/storyblok/RichTextBlock/RichTextBlock.stories.tsx
+++ packages/system/src/components/storyblok/RichTextBlock/RichTextBlock.stories.tsx
@@ -1,14 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react"
+import { GridMaster } from "../../ui/GridMaster"
 import { RichTextBlock } from "./RichTextBlock"
-import {
-  mockArticleRichTextBlok,
-  mockBasicRichTextBlok,
-  mockEmptyRichTextBlok,
-  mockListRichTextBlok,
-  mockNoContentRichTextBlok,
-  mockQuoteRichTextBlok,
-  mockShortRichTextBlok,
-} from "./RichTextBlock.mocks"
+import { mockBasicRichTextBlok } from "./RichTextBlock.mocks"
 
 const meta: Meta<typeof RichTextBlock> = {
   title: "Storyblok/RichTextBlock",
@@ -19,75 +12,34 @@ const meta: Meta<typeof RichTextBlock> = {
       defaultViewport: "desktop",
     },
   },
-  argTypes: {
-    blok: { control: "object" },
+  args: {
+    blok: mockBasicRichTextBlok,
   },
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <GridMaster>
+        <main>
+          <Story />
+        </main>
+      </GridMaster>
+    ),
+  ],
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Basic: Story = {
-  args: {
-    blok: mockBasicRichTextBlok,
+export const Default: Story = {}
+
+export const Tablet: Story = {
+  globals: {
+    viewport: { value: "tablet", isRotated: false },
   },
 }
 
-export const Article: Story = {
-  args: {
-    blok: mockArticleRichTextBlok,
-  },
-}
-
-export const WithLists: Story = {
-  args: {
-    blok: mockListRichTextBlok,
-  },
-}
-
-export const WithQuote: Story = {
-  args: {
-    blok: mockQuoteRichTextBlok,
-  },
-}
-
-export const Short: Story = {
-  args: {
-    blok: mockShortRichTextBlok,
-  },
-}
-
-export const Empty: Story = {
-  args: {
-    blok: mockEmptyRichTextBlok,
-  },
-}
-
-export const NoContent: Story = {
-  args: {
-    blok: mockNoContentRichTextBlok,
-  },
-}
-
-export const TabletView: Story = {
-  args: {
-    blok: mockArticleRichTextBlok,
-  },
-  parameters: {
-    viewport: {
-      defaultViewport: "tablet",
-    },
-  },
-}
-
-export const MobileView: Story = {
-  args: {
-    blok: mockArticleRichTextBlok,
-  },
-  parameters: {
-    viewport: {
-      defaultViewport: "mobile1",
-    },
+export const Mobile: Story = {
+  globals: {
+    viewport: { value: "mobile2", isRotated: false },
   },
 }

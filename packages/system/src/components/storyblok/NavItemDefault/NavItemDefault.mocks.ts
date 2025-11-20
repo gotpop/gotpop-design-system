2193b2ccb54bb2e10f9a103a5dd0f15417d6248f
GotPop <gotpop@users.noreply.github.com>
2025-11-20 15:47:29 +0000

    Removing mocks

--- packages/system/src/components/storyblok/NavItemDefault/NavItemDefault.mocks.ts
+++ packages/system/src/components/storyblok/NavItemDefault/NavItemDefault.mocks.ts
@@ -1,23 +1,9 @@
-import type {
-  ConfigStoryblok,
-  NavItemDefaultStoryblok,
-} from "../../../types/storyblok-components"
+import type { NavItemDefaultStoryblok } from "../../../types/storyblok-components"
 
-// Mock config
-export const mockConfig: ConfigStoryblok = {
-  _uid: "config-1",
-  component: "Config",
-  app_name: "Lorem Ipsum",
-  description: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
-  root_name_space: "blog",
-  primary_content_name_space: "blog",
-}
-
-// Text-only navigation items
 export const mockTextOnlyNavItem: NavItemDefaultStoryblok = {
   _uid: "nav-item-text-1",
   component: "nav_item_default",
-  text: "Lorem Ipsum",
+  text: "Lorem",
   type: "text",
   link: {
     cached_url: "/about",
@@ -25,23 +11,10 @@ export const mockTextOnlyNavItem: NavItemDefaultStoryblok = {
   },
 }
 
-// Icon-only navigation items
-export const mockIconOnlyNavItem: NavItemDefaultStoryblok = {
-  _uid: "nav-item-icon-1",
-  component: "nav_item_default",
-  icon: "home",
-  type: "icon",
-  link: {
-    cached_url: "/",
-    linktype: "story",
-  },
-}
-
-// Icon + text navigation items
 export const mockIconTextNavItem: NavItemDefaultStoryblok = {
   _uid: "nav-item-both-1",
   component: "nav_item_default",
-  text: "Dolor Sit",
+  text: "Dolor",
   icon: "dashboard",
   type: "icon-text",
   link: {
@@ -50,7 +23,6 @@ export const mockIconTextNavItem: NavItemDefaultStoryblok = {
   },
 }
 
-// External link navigation item
 export const mockExternalNavItem: NavItemDefaultStoryblok = {
   _uid: "nav-item-external-1",
   component: "nav_item_default",
@@ -61,49 +33,5 @@ export const mockExternalNavItem: NavItemDefaultStoryblok = {
     url: "https://github.com",
     linktype: "url",
     target: "_blank",
-  },
-}
-
-// Disabled/no-link navigation item
-export const mockDisabledNavItem: NavItemDefaultStoryblok = {
-  _uid: "nav-item-disabled-1",
-  component: "nav_item_default",
-  text: "Adipiscing",
-  type: "disabled",
-  // No link or invalid link to demonstrate disabled state
-}
-
-// Various navigation item scenarios
-export const mockPrimaryNavItem: NavItemDefaultStoryblok = {
-  _uid: "nav-item-primary-1",
-  component: "nav_item_default",
-  text: "Elit",
-  type: "primary",
-  link: {
-    cached_url: "/",
-    linktype: "story",
-  },
-}
-
-export const mockSecondaryNavItem: NavItemDefaultStoryblok = {
-  _uid: "nav-item-secondary-1",
-  component: "nav_item_default",
-  text: "Tempor",
-  type: "secondary",
-  link: {
-    cached_url: "/blog",
-    linktype: "story",
-  },
-}
-
-export const mockSettingsNavItem: NavItemDefaultStoryblok = {
-  _uid: "nav-item-settings-1",
-  component: "nav_item_default",
-  text: "Incididunt",
-  icon: "settings",
-  type: "utility",
-  link: {
-    cached_url: "/settings",
-    linktype: "story",
   },
 }
